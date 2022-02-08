f2be3113722a60d7bddba711c80b57a0f203db7c
anmol1357 <99263494+anmol1357@users.noreply.github.com>
2022-02-08 18:54:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var box
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(100,200,100,100);
+  box.shapeColor="red"
+}
+
+function draw() 
+{
+  background(30);
+  if(keyIsDown(RIGHT_ARROW)){
+    box.x=box.x+5
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.x=box.x-5
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.y=box.y-5
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.y=box.y+5
+  }
+
+  drawSprites();
+  
+
+}
+
+
+
+
